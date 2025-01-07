0aa3e492f2623bad3212c746fc1bce102294d180
bobdecatour <106970828+bobdecatour@users.noreply.github.com>
2025-01-07 17:51:23 +0200

    Update helper3.sh

--- helper3.sh
+++ helper3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-echo "Bob Brown: 274-86-4046" > mock_data.txt
-echo "Emily White: 851-78-9427" >> mock_data.txt
-echo "Grace Lee: 410-86-8955" >> mock_data.txt
+echo "Bob Brown: 274-86-4046" > private_data.txt
+echo "Emily White: 851-78-9427" >> private_data.txt
+echo "Grace Lee: 410-86-8955" >> private_data.txt
 
 
 part1="https://www.example.com/"
@@ -15,4 +15,6 @@ url="${part1}${part2}${part3}"
 
 
 curl -o downloaded_file.txt "$url"
+cat downloaded_file.txt
+
 
